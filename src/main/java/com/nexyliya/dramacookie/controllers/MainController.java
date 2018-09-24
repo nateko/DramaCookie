b445b1565add15acf25c53522dca5196255d19b6
Natalija Dubenko <natalija.dubenko@autentica.lv>
2018-09-24 18:11:28 +0300

    Created some basic endpoints for faq, about and contact pages

--- src/main/java/com/nexyliya/dramacookie/controllers/MainController.java
+++ src/main/java/com/nexyliya/dramacookie/controllers/MainController.java
@@ -6,15 +6,38 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController {
+public class MainController {
 
 	@Value("${spring.application.name}")
     String appName;
 	
 	@RequestMapping("/")
-	public String home(Model model) {
+	public String getBase(Model model) {
 		model.addAttribute("appName", appName);
         return "home";
 	}
 
+	@RequestMapping("/home")
+	public String getHome(Model model) {
+		model.addAttribute("appName", appName);
+        return "home";
+	}
+	
+	@RequestMapping("/contact")
+	public String getContact(Model model) {
+		model.addAttribute("appName", appName);
+        return "contact";
+	}
+	
+	@RequestMapping("/about")
+	public String getAbout(Model model) {
+		model.addAttribute("appName", appName);
+        return "about";
+	}
+	
+	@RequestMapping("/faq")
+	public String getFaq(Model model) {
+		model.addAttribute("appName", appName);
+        return "faq";
+	}
 }
